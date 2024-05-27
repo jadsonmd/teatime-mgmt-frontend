a5002c8d1bca18e2aed82169a3fc60ed43992c51
Jadson Mezzari Dagostin <jadsonmd@icloud.com>
2024-05-27 16:32:45 -0300

    Login page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { AppComponent } from './app/app.component';
+
+appConfig.providers.push(provideAuth0({
+  domain: 'YOUR_DOMAIN',
+  clientId: 'YOUR_CLIENT',
+  authorizationParams: {
+    redirect_uri: window.location.origin
+  }
+}));
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
