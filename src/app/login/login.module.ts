38e354db9d4b8f44b4e9325da65a740d5b5430b0
Jadson Mezzari Dagostin <jadsonmd@icloud.com>
2024-08-30 15:55:33 -0300

    Adicionando sistema solar no projeto

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { SolarSystemModule } from '../component/solar-system/solar-system.module';
 
 
 
@@ -17,7 +18,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule, 
     MatInputModule, 
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    SolarSystemModule
   ],
 })
 export class LoginModule { }
